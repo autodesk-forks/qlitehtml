48824ded2f9b0bad714e041a1866820c68a80677
Jarek Kobus <jaroslaw.kobus@qt.io>
2021-04-20 15:13:39 +0200

    Update license headers again
    Change-Id: I145e79bfc475dd64abd1d2dc7974788330ea7c8c
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/qlitehtml_global.h
+++ src/qlitehtml_global.h
@@ -17,7 +17,7 @@
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
 ** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
 ** included in the packaging of this file. Please review the following
 ** information to ensure the GNU General Public License requirements will
 ** be met: https://www.gnu.org/licenses/gpl-3.0.html.
