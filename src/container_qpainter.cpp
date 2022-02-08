1d73f76faceb4b70b4eedab110400db7fddf6eb9
Evgeniy A. Dushistov <dushistov@mail.ru>
2022-02-08 12:35:04 +0300

    Fix parsing of float numbers with different locales
    This MR merge last changes from litehtml, mainly for
    https://github.com/litehtml/litehtml/commit/bb04a50f93e94de0aa9a0be0b9b6e7435051dbf6
    to fix parsing CSS on machines with locales where "." is not decimial
    delimiter. For example in french locale "," used for decimial delimter.
    
    Change-Id: I8d50ed25e9717224078eaf48150c02281eeb2ae5
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/container_qpainter.cpp
+++ src/container_qpainter.cpp
@@ -558,7 +558,7 @@ void DocumentContainerPrivate::draw_text(litehtml::uint_ptr hdc,
     painter->drawText(toQRect(pos), 0, QString::fromUtf8(text));
 }
 
-int DocumentContainerPrivate::pt_to_px(int pt)
+int DocumentContainerPrivate::pt_to_px(int pt) const
 {
     // magic factor of 11/12 to account for differences to webengine/webkit
     return m_paintDevice->physicalDpiY() * pt * 11 / m_paintDevice->logicalDpiY() / 12;
